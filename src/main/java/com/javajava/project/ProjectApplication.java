ef7c44e3c7704051331436b7192bebd784bc0975
jongkwon0918 <whdrnjs0918@naver.com>
2026-03-17 23:34:13 +0900

    DataSourceAutoConfiguration 주석

--- src/main/java/com/javajava/project/ProjectApplication.java
+++ src/main/java/com/javajava/project/ProjectApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 // exclude 속성을 사용하여 데이터소스 자동 설정을 제외합니다.
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication //(exclude = {DataSourceAutoConfiguration.class}) -> DB연결하기 위해 잠시 주석처리
 public class ProjectApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProjectApplication.class, args);
