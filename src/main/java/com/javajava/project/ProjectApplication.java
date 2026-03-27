27d70c5f86f65a44df0222e12731c6014df28c0a
ohsuhwan <almond020418@gmail.com>
2026-03-27 19:11:10 +0900

    오수환 : 환경변수 라이브러리 사용방식으로 변경(ENV_SETUP.md)참조

--- src/main/java/com/javajava/project/ProjectApplication.java
+++ src/main/java/com/javajava/project/ProjectApplication.java
@@ -1,13 +1,14 @@
 package com.javajava.project;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-// exclude 속성을 사용하여 데이터소스 자동 설정을 제외합니다.
-@SpringBootApplication //(exclude = {DataSourceAutoConfiguration.class}) -> DB연결하기 위해 잠시 주석처리
+@SpringBootApplication
 public class ProjectApplication {
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        dotenv.entries().forEach(e -> System.setProperty(e.getKey(), e.getValue()));
         SpringApplication.run(ProjectApplication.class, args);
     }
 }
