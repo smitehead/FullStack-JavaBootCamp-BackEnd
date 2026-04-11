9c48f8e377f57e8b6f97612ce96c8c8e50fc272c
smitehead <dkdldjdy12@naver.com>
2026-04-11 11:58:15 +0900

    이미지 테이블 분리함에 따라 기존 이미지테이블 삭제

--- src/main/java/com/javajava/project/domain/product/entity/Image.java
+++
@@ -1,7 +0,0 @@
-package com.javajava.project.domain.product.entity;
-
-// 구버전 IMAGE 테이블 엔티티 — 신규 ERD에서는 PRODUCT_IMAGE / REPORT_IMAGE / INQUIRY_IMAGE 로 분리됨
-// JPA 매핑 제거 (Hibernate가 IMAGE 테이블을 생성하지 않도록)
-// 참조하는 코드가 없으므로 이 파일은 사용되지 않음
-public class Image {
-}
