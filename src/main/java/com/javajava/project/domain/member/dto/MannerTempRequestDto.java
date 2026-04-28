6c9b619da8a39571042966893955f6f216198a58
OHSUHWAN <almond020418@gmail.com>
2026-04-28 21:41:56 +0900

    오수환 : 매너온도 설정 로직 변경

--- src/main/java/com/javajava/project/domain/member/dto/MannerTempRequestDto.java
+++ src/main/java/com/javajava/project/domain/member/dto/MannerTempRequestDto.java
@@ -1,5 +1,7 @@
 package com.javajava.project.domain.member.dto;
 
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
@@ -11,6 +13,8 @@ import lombok.*;
 public class MannerTempRequestDto {
 
     @NotNull(message = "변경할 매너온도는 필수입니다.")
+    @DecimalMin(value = "0.0", message = "매너온도는 0도 이상이어야 합니다.")
+    @DecimalMax(value = "100.0", message = "매너온도는 100도 이하이어야 합니다.")
     private Double newTemp;
 
     @NotBlank(message = "변경 사유는 필수입니다.")
